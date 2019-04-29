58edb2f0c23f8140f28fef696ad2566bfd03fbfd
aymericfloyrac <aymeric.floyrac@polytechnique.edu>
2019-04-29 11:44:33 +0200

    Add files via upload

--- WallStreet.R
+++ WallStreet.R
@@ -22,15 +22,15 @@ k<-20 #longueur du MB
 n<-1000 #nombre de simulations
 W <- rnorm(n=n*k,mean = 0,sd=1/k)
 W<-matrix(W,nrow=n,ncol=k)
-W<- apply(W,2,cumsum)
+W<- t(apply(W,1,cumsum))
 dim(W)
 
-plot(W[,1],type='l',xlim = c(0,20),ylim = c(-3,3))
+plot(W[1,],type='l',xlim = c(0,20),ylim = c(-3,3))
 for (i in 2:n){
-  lines(W[,i])
+  lines(W[i,])
 }
 
-plot(colMeans(W))
+plot(colMeans(W),type='l')
 
 #dans un deuxieme temps on peut faire une construction brownian bridge 
 
@@ -83,8 +83,8 @@ monteCarlo<-function(N=1000,T=1,r=0.05,k=20,K=5,sigma=0.3,mu,antithet=T){
   else{
     C <- exp(-r*T)*pmax(rowMeans(S1)-K,0)
   }
-  beta <- cov(W1[,k],C)/var(W1[,k])
-  C <- C-beta*W1[,k]
+  #beta <- cov(W1[,k],C)/var(W1[,k])
+  #C <- C-beta*W1[,k]
   #return (list('mean'=mean(C),'sd'=sd(C)))
   return (c(mean(C),sd(C)))
 }
@@ -130,24 +130,30 @@ control()
 #mais on va utiliser la librairie déjà faite
 library(randtoolbox)
 # on va utiliser les fonctions halton, torus et sobol pour générer des gaussiennes
+# pour faire le RQMC on utilise l'option scrambling de la fonction sobol
 
-qmc<-function(N=1000,T=1,r=0.05,k=20,K=5,sigma=0.3,mu,funcStr='halton',antithet=T){
+qmc<-function(N=1000,T=1,r=0.05,k=20,K=5,sigma=0.3,mu,funcStr='halton',antithet=T,randomized=F){
   if (missing(mu)){
     mu<-r
   } #on suppose que, si non spécifié, le drift et le taux d'intérêt sont égaux
   t <- (1:k)/k
-  switch (funcStr,
-    halton={
-      W <- (1/k)*halton(normal=T,n = N,dim = k-1)
-    },
-    torus={
-      W <- (1/k)*torus(normal=T,n = N,dim = k-1)
-    },
-    sobol={
-      W <- (1/k)*sobol(normal=T,n = N,dim = k-1)
-    },
-    print('qmc function not specified')
-  )
+  if (randomized){
+    W <- (1/k)*sobol(normal=T,n = N,dim = k-1,scrambling = 3)
+  }
+  else{
+    switch (funcStr,
+      halton={
+        W <- (1/k)*halton(normal=T,n = N,dim = k-1)
+      },
+      torus={
+        W <- (1/k)*torus(normal=T,n = N,dim = k-1)
+      },
+      sobol={
+        W <- (1/k)*sobol(normal=T,n = N,dim = k-1)
+      },
+      print('qmc function not specified')
+    )
+  }
   
   W<-t(apply(W,1,cumsum))
   W1 <- c(rep(0,N),W)
@@ -170,18 +176,17 @@ qmc<-function(N=1000,T=1,r=0.05,k=20,K=5,sigma=0.3,mu,funcStr='halton',antithet=
 }
 qmc(funcStr = 'sobol')
 
-############# RANDOMIZED QMC ###############
-#aucune idée 
 
 ############# RECAPITULONS ###############
-results<-data.frame(matrix(nrow=5,ncol=2),row.names = c('mc','antithetic','ctrl','qmc','qmc antithetic'))
+results<-data.frame(matrix(nrow=7,ncol=2),row.names = c('mc','antithetic','ctrl','qmc','qmc antithetic','rqmc','rqmc anti'))
 colnames(results)<-c('moyenne','ecart-type')
-results[1,]<-monteCarlo(antithet=F)
+results[1,]<-monteCarlo(antithet=F,N=10000)
 results[2,]<-monteCarlo(antithet = T)
 results[3,]<-control()
-results[4,]<-qmc(antithet = F)
+results[4,]<-qmc(antithet = F,N=10000)
 results[5,]<-qmc(antithet = T)
-
+results[6,]<-qmc(antithet = F,randomized = T)
+results[7,]<-qmc(antithet = T,randomized = T)
 results
 
 #on a donc pas la même moyenne selon les méthodes, ce qui est gênant, 
@@ -200,20 +205,19 @@ multiLevel<-function(N,epsilon,T=1,r=0.05,sigma=0.3,K=5,mu){
   
   g <- rnorm(n = N*M0)
   g <- matrix(g,nrow=N,ncol=M0)
-  Sg<-1*(1+mu*h0+sigma*sqrt(h0)*g)
-  estim <- colMeans(pmax(S,0)) #à voir si c'est vraiment un colmean
-  
+  Sg<-K*(1+mu*h0+sigma*sqrt(h0)*g) #approximation lineaire
+  estim <- rowMeans(pmax(Sg-K,0)) #à voir si c'est vraiment un rowmean
   for (l in 1:L){
     Ml <- as.integer(M0/2**l)
     h<-h0/2**l
     sig <- sigma*sqrt(h)
-    Sf <- matrix(1,nrow=N,ncol=Ml)
-    Sg <- matrix(1,nrow=N,ncol=Ml)
+    Sf <- matrix(1,nrow=N,ncol=Ml) #schema d'euler de pas fin
+    Sg <- matrix(1,nrow=N,ncol=Ml) #schema d'euler de pas grossier
     
     for (k in 1:2**(l-1)){
       g1 <- rnorm(n=N*Ml)
       g2 <- rnorm(n=N*Ml)
-      
+      #evolution de deux schemas: pas fin et pas grossier
       Sf <- Sf*(1+mu*h+sig*g1)
       Sf <- Sf*(1+mu*h+sig*g2)
       Sg <- Sg*(1+2*mu*h+sig*(g1+g2))
@@ -225,5 +229,6 @@ multiLevel<-function(N,epsilon,T=1,r=0.05,sigma=0.3,K=5,mu){
   return (estim)
 }
 
-multiLevel(N=1000,epsilon = 0.01)
-
+e<-multiLevel(N=1000,epsilon = 0.1)
+mean(e)
+sd(e)
